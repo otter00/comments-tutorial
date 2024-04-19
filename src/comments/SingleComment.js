e9fe9cfb8170652a05ebe1442f25bf8d24487c90
otter00 <ivlevanastia2001@mail.ru>
2024-04-19 22:21:33 +0300

    editing func typo fixed

--- src/comments/SingleComment.js
+++ src/comments/SingleComment.js
@@ -65,7 +65,7 @@ const SingleComment = ({
             <div
               className="comment-action"
               onClick={() =>
-                setActiveComment({ id: comment.id, type: "editiing" })
+                setActiveComment({ id: comment.id, type: "editing" })
               }
             >
               Edit
